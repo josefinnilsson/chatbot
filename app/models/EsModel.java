f2108304257f3a519a89430941079e88e30b24df
martin <mhyberg@kth.se>
2017-04-04 12:57:28 +0200

    added EsModel skeleton

---
+++ app/models/EsModel.java
@@ -0,0 +1,40 @@
+package models;
+
+public class EsModel extends Model{
+
+
+    /*
+    This model talks to elasticsearch
+    */
+    public EsModel(){
+    //TODO
+    }
+
+    /*
+    Sends a query to ES
+    @param The query you want to ask ES
+    @return the search responnse from ES
+    */
+    private void getResponse(String query){
+        //TODO
+    }
+
+    /*
+    Returns the answers from an ES query
+    @return all answers from ES
+    */
+    public String getAllAnswers(){
+        //TODO
+    }
+
+    /*
+    Returns the first answers
+    @param a response from ES
+    @return a single answer
+    */
+    public String getAnswer(Response response){
+        //TODO
+    }
+
+
+}
