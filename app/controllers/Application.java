27b25db52f72b1f35f002d5738aea8cd787b065f
Samuel Hertzberg <shert@kth.se>
2017-04-04 13:34:28 +0200

    Added new controller for ES and with it some test responses, also added ES dependencies and changed some routing back and forth

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,36 +1,38 @@
-package controllers; 
+package controllers;
 
-import play.*; 
-import play.mvc.*; 
+import play.*;
+import play.mvc.*;
 import play.data.Form;
 import models.*;
 import views.html.*;
-import static play.libs.Json.toJson; 
+import static play.libs.Json.toJson;
 import java.util.*;
-import com.avaje.ebean.Model;  
-import javax.inject.Inject; 
-import play.data.FormFactory; 
-import play.data.DynamicForm; 
+import com.avaje.ebean.Model;
+import javax.inject.Inject;
+import play.data.FormFactory;
+import play.data.DynamicForm;
 
 public class Application extends Controller {
 
 	@Inject
-	private FormFactory formFactory; 
+	private FormFactory formFactory;
+	private ElasticsearchController esController = new ElasticsearchController();
 
 	public Result index() {
-		return ok(index.render("Index")); 
+		return ok(index.render("Index"));
 	}
-	
+
 	public Result addMessage() {
-		Form<Message> messageForm = formFactory.form(Message.class); 
-		Message message = messageForm.bindFromRequest().get(); 
-		message.save(); 
-		return redirect(routes.Application.index()); 
+		Form<Message> messageForm = formFactory.form(Message.class);
+		Message message = messageForm.bindFromRequest().get();
+		message.save();
+		esController.Query(message);
+		return redirect(routes.Application.index());
 	}
 
 	public Result getMessages() {
-		Model.Finder<Integer, Message> finder = new Model.Finder<>(Message.class); 
-		List<Message> messages = finder.all(); 
-		return ok(toJson(messages));  
+		Model.Finder<Integer, Message> finder = new Model.Finder<>(Message.class);
+		List<Message> messages = finder.all();
+		return ok(toJson(messages));
 	}
 }
--- app/models/Message.java
+++ app/models/Message.java
@@ -1,21 +1,34 @@
-package models; 
+package models;
 
-import java.util.*; 
-import javax.persistence.*;  
-import com.avaje.ebean.Model; 
-import play.data.format.*; 
-import play.data.validation.*; 
+import java.util.*;
+import javax.persistence.*;
+import com.avaje.ebean.Model;
+import play.data.format.*;
+import play.data.validation.*;
 
 @Entity
 public class Message extends Model {
 	@Id
-	public Integer id; 
+	public Integer id;
 
-	public String name; 
+	public String name;
 
-	public Message() {}
+	public Message(){}
 
 	public Message(String name) {
-		this.name = name; 
+		this.name = name;
 	}
+
+	public String getName(){
+		return name;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public void setMessage(String name){
+		this.name = name;
+	}
+
 }
