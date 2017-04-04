d927f0cdc51a9c9f374182bba5b836bfddf8b299
Samuel Hertzberg <shert@kth.se>
2017-04-04 14:14:38 +0200

    Rewrote some parts of the query call to the ES-Model, added comments

---
+++ app/controllers/ElasticsearchController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.Form;
+import models.*;
+import views.html.*;
+import static play.libs.Json.toJson;
+import java.util.*;
+import com.avaje.ebean.Model;
+import javax.inject.Inject;
+import play.data.FormFactory;
+import play.data.DynamicForm;
+
+/*
+This controller talks with the Elasticsearch model.
+*/
+public class ElasticsearchController extends Controller {
+  EsModel esModel = new EsModel();
+
+  public ElasticsearchController(){
+  }
+
+  /**
+  Creates a new message with the return from Elasticsearch and saves it.
+  @param message model with message from the user.
+  */
+  public void Query(Message msg){
+    //Message to return
+    Message message = new Message();
+    String queryString = msg.getName();
+    //Get queryResult as string from ES-Model
+    String queryResult = esModel.getAnswer(queryString);
+    //Set return-message text to query result
+    message.setMessage(queryResult
+    //Save return-message for view
+    message.save();
+  }
+}
